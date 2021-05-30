6824d2d6e94e151c55491633dd334531f3b29bce
RinozaJiffry <66359174+RinozaJiffry@users.noreply.github.com>
2021-05-30 18:29:13 +0530

    java

---
+++ loop.java
@@ -0,0 +1,24 @@
+public class loop
+{
+    public static void main(String[] args)
+    {
+        int num = 40;
+
+        boolean number = 38<28;
+        boolean ext = 30 < 50;
+
+        boolean k = num > 50;
+
+        System.out.println(number);
+        System.out.println(ext);
+        System.out.println(k);
+
+        while(num < 60)
+        {
+            System.out.println("Value is less than 60 => " + num);
+            num = num +2;
+        }
+            System.out.println("Value is greater than 60");
+
+    }
+}
