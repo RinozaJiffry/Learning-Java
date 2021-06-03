09a9cbf578741fcf117c26962857d43e4fd6bf4e
RinozaJiffry <66359174+RinozaJiffry@users.noreply.github.com>
2021-06-04 00:03:34 +0530

    java

---
+++ learningForLoop.java
@@ -0,0 +1,11 @@
+public class learningForLoop
+{
+    public static void main(String[] args)
+    {
+        int i;
+        for(i = 1; i <=5; i++){
+            System.out.println("Hello " + i);
+        }
+        System.out.println("Hello " + i);
+    }
+}
